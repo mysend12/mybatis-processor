792d05facb3a0bec2ef7dab23830fae040b800ca
mysend12 <mysend12@naver.com>
2020-11-12 20:49:25 +0900

    #34-How to use @ResultMap annotation example

--- demo/src/main/java/io/my/demo/repository/UserRepository.java
+++ demo/src/main/java/io/my/demo/repository/UserRepository.java
@@ -3,10 +3,16 @@ package io.my.demo.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
 
 import io.my.demo.entity.User;
 
 @Mapper
 public interface UserRepository {
     List<User> selectUsers();
+
+    @ResultMap("userResultMap")
+    @Select("SELECT * FROM `USER_INFO` ui JOIN `USER` u ON u.ID = ui.USER_ID JOIN USER_GROUP ug ON u.USER_GROUP_ID = ug.ID ORDER BY ug.GROUP_NAME ASC")
+    List<User> selectUserList();
 }
